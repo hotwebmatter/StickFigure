1617f028d820295ab7c81d07cdf448ea9a075c47
Matt Obert on Windows <matt@hotwebmatter.com>
2019-09-02 13:47:25 -0400

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace StickFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tiny stick figure
+            // (because I already understand character escape sequences)
+            WriteLine();
+            WriteLine("\\o/");
+            WriteLine(" |");
+            WriteLine("/ \\");
+            WriteLine();
+            ReadKey();
+        }
+    }
+}
